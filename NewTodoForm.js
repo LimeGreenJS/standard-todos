0c6c6c92da0349e06d6780a6b099ba966db1c9fa
Daishi Kato <dai-shi@users.noreply.github.com>
2018-04-13 23:12:04 +0900

    Update NewTodoForm.js

--- NewTodoForm.js
+++ NewTodoForm.js
@@ -1,37 +1,8 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 
 import { QUERY_ALL_TASKS, getQueryAllTasksVariables } from './TodoList';
-
-const NewTodoForm = ({ userInfo, mutate }) => {
-  const onClick = (event) => {
-    event.preventDefault();
-    const title = event.target.parentNode.title.value;
-    if (!title) return;
-    const ownerId = userInfo && userInfo.id;
-    const description = event.target.parentNode.description.value;
-    const isPrivate = ownerId && event.target.parentNode.isPrivate.checked;
-    mutate({ variables: { title, description, ownerId, isPrivate } });
-  };
-  return (
-    <form className="new-task">
-      <input name="title" placeholder="Enter title..." />
-      <input name="description" placeholder="Enter description..." />
-      {userInfo && (
-        <div>
-          <input
-            name="isPrivate"
-            type="checkbox"
-            style={{ width: 'inherit', marginBottom: 20 }}
-          />
-          private
-        </div>
-      )}
-      <button type="submit" onClick={onClick}>Add</button>
-    </form>
-  );
-};
 
 const CREATE_TASK = gql`
 mutation createTask($title: String!, $description: String, $ownerId: ID, $isPrivate: Boolean) {
@@ -48,17 +19,45 @@ mutation createTask($title: String!, $description: String, $ownerId: ID, $isPriv
 }
 `;
 
-const config = {
-  options: ({ userInfo }) => {
-    const variables = getQueryAllTasksVariables(userInfo);
-    return {
-      update: (proxy, { data: { createTask } }) => {
-        const data = proxy.readQuery({ query: QUERY_ALL_TASKS, variables });
-        data.allTasks.unshift(createTask);
-        proxy.writeQuery({ query: QUERY_ALL_TASKS, variables, data });
-      },
-    };
-  },
-};
+const NewTodoForm = ({ userInfo }) => (
+  <Mutation
+    mutation={CREATE_TASK}
+    update={(cache, { data: { createTask } }) => {
+      const data = cache.readQuery({ query: QUERY_ALL_TASKS, variables });
+      data.allTasks.unshift(createTask);
+      const variables = getQueryAllTasksVariables(userInfo);
+      cache.writeQuery({ query: QUERY_ALL_TASKS, variables, data });
+    }}
+  >
+    {(createTask) => {
+      const onClick = (event) => {
+        event.preventDefault();
+        const title = event.target.parentNode.title.value;
+        if (!title) return;
+        const ownerId = userInfo && userInfo.id;
+        const description = event.target.parentNode.description.value;
+        const isPrivate = ownerId && event.target.parentNode.isPrivate.checked;
+        createTask({ variables: { title, description, ownerId, isPrivate } });
+      };
+      return (
+        <form className="new-task">
+          <input name="title" placeholder="Enter title..." />
+          <input name="description" placeholder="Enter description..." />
+          {userInfo && (
+            <div>
+              <input
+                name="isPrivate"
+                type="checkbox"
+                style={{ width: 'inherit', marginBottom: 20 }}
+              />
+              private
+            </div>
+          )}
+          <button type="submit" onClick={onClick}>Add</button>
+        </form>
+      );
+    }}
+  </Mutation>
+);
 
-export default graphql(CREATE_TASK, config)(NewTodoForm);
+export default NewTodoForm;
