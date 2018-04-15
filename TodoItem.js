e580d16cd0bd5919dcdc6f9721e62eb956d760e0
Daishi Kato <dai-shi@users.noreply.github.com>
2018-04-15 09:38:40 +0900

    use Mutation

--- TodoItem.js
+++ TodoItem.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 
 import { QUERY_ALL_TASKS, getQueryAllTasksVariables } from './TodoList';
-
-const TodoItem = ({ userInfo, item, mutate }) => {
-  const onClick = (event) => {
-    mutate({ variables: { id: item.id } });
-  };
-  const { id } = userInfo || {};
-  return (
-    <div>
-      {(!item.owner || item.owner.id === id) &&
-        <button className="delete" onClick={onClick}>&times;</button>}
-      {item.owner && <strong>{item.owner.email}&nbsp;</strong>}
-      {item.title} - {item.description}
-    </div>
-  );
-};
 
 const DELETE_TASK = gql`
 mutation deleteTask($id: ID!) {
@@ -27,20 +12,34 @@ mutation deleteTask($id: ID!) {
 }
 `;
 
-const config = {
-  options: ({ userInfo }) => {
-    const variables = getQueryAllTasksVariables(userInfo);
-    return {
-      update: (proxy, { data: { deleteTask } }) => {
-        const data = proxy.readQuery({ query: QUERY_ALL_TASKS, variables });
-        const index = data.allTasks.findIndex(task => task.id === deleteTask.id);
-        if (index >= 0) {
-          data.allTasks.splice(index, 1);
-          proxy.writeQuery({ query: QUERY_ALL_TASKS, variables, data });
-        }
-      },
-    };
-  },
-};
+const TodoItem = ({ userInfo, item }) => (
+  <Mutation
+    mutation={DELETE_TASK}
+    update={(cache, { data: { deleteTask } }) => {
+      const variables = getQueryAllTasksVariables(userInfo);
+      const data = cache.readQuery({ query: QUERY_ALL_TASKS, variables });
+      const index = data.allTasks.findIndex(task => task.id === deleteTask.id);
+      if (index >= 0) {
+        data.allTasks.splice(index, 1);
+        cache.writeQuery({ query: QUERY_ALL_TASKS, variables, data });
+      }
+    }}
+  />
+    {(deleteTask) => {
+      const onClick = (event) => {
+        deleteTask({ variables: { id: item.id } });
+      };
+      const { id } = userInfo || {};
+      return (
+        <div>
+          {(!item.owner || item.owner.id === id) &&
+            <button className="delete" onClick={onClick}>&times;</button>}
+          {item.owner && <strong>{item.owner.email}&nbsp;</strong>}
+          {item.title} - {item.description}
+        </div>
+      );
+    }}
+  </Mutation>
+);
 
-export default graphql(DELETE_TASK, config)(TodoItem);
+export default TodoItem;
